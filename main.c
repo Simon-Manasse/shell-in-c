99eceaaa3f2be81a68930489d52cf2f6c97d7ddd
Simon-Manasse <simo0047@hz.nl>
2025-02-12 18:04:30 +0000

    read in prompt

--- main.c
+++ main.c
@@ -1,3 +1,41 @@
+#define MACSKA_RL_BUFSIZE 1024
+
+char *macska_read_line(void){
+  int bufsize = MACSKA_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char)* bufsize);
+  int c;
+
+  if (!buffer){
+    fprintf(stderr, "macska: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  while (1){
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c=='\n'){
+      buffer[position] = '\0';
+      return buffer;
+    }
+    else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if(position >=bufsize){
+      bufsize+= MACSKA_RL_BUFSIZE;
+      buffer = realloc(buffer,bufsize);
+      if (!buffer){
+        fprintf(stderr, "macska: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
 void macska_loop(void){
   char *line;
   char **args;
@@ -10,7 +48,7 @@ void macska_loop(void){
     status = macska_execute(args);
 
     free(line);
-    free(args);
+    free(args)
   } while (status);
 }
 
